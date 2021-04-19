37c9a32bd316af4e9af9cbcbc1606e6b6fc6a002
Grant Gardner <youngheart80@gmail.com>
2021-04-18 22:56:50 -0500

    Update rules.mk

--- keyboards/kyria/keymaps/youngheart80/rules.mk
+++ keyboards/kyria/keymaps/youngheart80/rules.mk
@@ -1,6 +1,6 @@
 OLED_DRIVER_ENABLE = no    # Enables the use of OLED displays
 ENCODER_ENABLE = no        # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow 
-LEADER_ENABLE = yes 		# Enable leader key functionality
+LEADER_ENABLE = no 		   # Enable leader key functionality
 
 MOUSEKEY_ENABLE = no       # Enable the use of the mouse with keystrokes
